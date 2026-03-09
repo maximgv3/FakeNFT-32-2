bc53531882feaa810cd89badb1097ad6e532a24a
Rustam Khanakhmedov <bateg@mail.ru>
2026-03-09 14:18:57 +0300

    Module 1: Верстка экрана корзины

--- iOS-FakeNFT-Extended/App/TabBarView.swift
+++ iOS-FakeNFT-Extended/App/TabBarView.swift
@@ -1,16 +1,63 @@
 import SwiftUI
 
 struct TabBarView: View {
+    @State private var selectedTab = 0
+    
     var body: some View {
-        TabView {
-            TestCatalogView()
+        TabView(selection: $selectedTab) {
+            // Профиль
+            Color.blue
+                .overlay(Text("Профиль").foregroundColor(.white))
                 .tabItem {
-                    Label(
-                        NSLocalizedString("Tab.catalog", comment: ""),
-                        systemImage: "square.stack.3d.up.fill"
-                    )
+                    VStack {
+                        Image("profile")
+                            .renderingMode(.template)
+                        Text("Профиль")
+                    }
                 }
-                .backgroundStyle(.background)
+                .tag(0)
+            
+            // Каталог
+            Color.green
+                .overlay(Text("Каталог").foregroundColor(.white))
+                .tabItem {
+                    VStack {
+                        Image("catalog")
+                            .renderingMode(.template)
+                        Text("Каталог")
+                    }
+                }
+                .tag(1)
+            
+            // Корзина
+            NavigationStack {
+                CartView(items: CartItem.mockItems)
+            }
+            .tabItem {
+                VStack {
+                    Image("cartTabItem")
+                        .renderingMode(.template)
+                    Text("Корзина")
+                }
+            }
+            .tag(2)
+            
+            // Статистика
+            Color.orange
+                .overlay(Text("Статистика").foregroundColor(.white))
+                .tabItem {
+                    VStack {
+                        Image("statistic")
+                            .renderingMode(.template)
+                        Text("Статистика")
+                    }
+                }
+                .tag(3)
         }
+        .tint(.ypUBlue)
     }
+}
+
+#Preview {
+    TabBarView()
 }
