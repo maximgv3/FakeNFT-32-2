66e7226625c1706ca0ef412603dffb225a5b0ca7
Maxim <mquesmark@icloud.com>
2026-03-20 20:37:08 +0300

    [Feature] Add update profile network request

--- iOS-FakeNFT-Extended/Core/Network/NetworkRequest.swift
+++ iOS-FakeNFT-Extended/Core/Network/NetworkRequest.swift
@@ -11,10 +11,14 @@ protocol NetworkRequest {
     var endpoint: URL? { get }
     var httpMethod: HttpMethod { get }
     var dto: Encodable? { get }
+    var headers: [String: String] { get }
+    var body: Data? { get }
 }
 
 // default values
 extension NetworkRequest {
     var httpMethod: HttpMethod { .get }
     var dto: Encodable? { nil }
+    var headers: [String: String] { [:] }
+    var body: Data? { nil }
 }
---
+++ iOS-FakeNFT-Extended/Core/Network/UpdateProfileRequest.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+struct UpdateProfileRequest: NetworkRequest {
+
+    var headers: [String: String] {
+        ["Content-Type": "application/x-www-form-urlencoded"]
+    }
+    var body: Data? {
+        var components: [String] = []
+
+        components.append("name=\(encode(profile.name))")
+        components.append("description=\(encode(profile.description ?? ""))")
+        components.append("avatar=\(encode(profile.avatar))")
+        components.append("website=\(encode(profile.website))")
+
+        for like in profile.likes {
+            components.append("likes=\(encode(like))")
+        }
+
+        return components.joined(separator: "&").data(using: .utf8)
+    }
+    
+    let id: String
+    let profile: Profile
+    let httpMethod: HttpMethod = .put
+    
+    var endpoint: URL? {
+        URL(string: "\(RequestConstants.baseURL)/api/v1/profile/\(id)")
+    }
+
+    private func encode(_ value: String) -> String {
+        value.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? value
+    }
+}
